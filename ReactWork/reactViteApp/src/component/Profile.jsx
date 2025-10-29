6399542796228fdf2d80d9fba750425390ebab69
Ankit Ydv <Ankityadav550>
2025-10-29 10:24:07 +0530

    React+Vite Component

---
+++ ReactWork/reactViteApp/src/component/Profile.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import './profile.css'
+
+function Profile() {
+  return (
+    <div className='parent'>
+        {/* <h2>Profile Page</h2> */}
+        <img src="https://wallpaperaccess.com/full/682452.jpg" alt="image" />
+        <div>
+            <h2>Ankit Yadav</h2>
+        <h2>CS-B</h2>
+        <h2>Abes engineering college</h2>
+        </div>
+        
+    </div>
+  )
+}
+
+export default Profile
