d5904cd069d625e4bfc0b19c584eb114ab77a55c
Ankityadav550 <ankitsahil1000@gmail.com>
2026-02-19 21:36:14 +0530

    react+backend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -13,7 +13,7 @@ function App() {
     const serverData = await fetch("http://localhost:4004/data")
     const data = await serverData.json()
     console.log(data)
-    setData(data)
+    setData(data.msg.products);
     }
     catch(err){
       console.log(err);
@@ -24,10 +24,25 @@ function App() {
    
   }
 
+  function cartItem(ele){
+    alert(ele);
+  }
+
   return (
     <>
       <h2>Fetching Data from node server</h2>
-      {JSON.stringify(data)}
+      {
+        data.map((ele)=>(
+          <div style ={{border:"1px solid black",margin:"10px",padding:"10px"}}>
+            <h2> {ele.id}:{ele.title}</h2>
+            <h3>{ele.description}</h3>
+            <h2>{ele.price}</h2>
+            <h2>{ele.rating}</h2>
+            <button onClick={()=>cartItem(ele)}>Add to cart</button>
+          </div>
+        ))
+      }
+      {/* {JSON.stringify(data)} */}
       <button onClick={getData}>FetchData</button>
       {
         loader ?(<p style={{color:"yellow"}}>Loading...</p>) : null
